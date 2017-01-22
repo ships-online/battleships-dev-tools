61e16a8fcdc22ad78b09ea7b2231628135992121
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-01-22 02:06:49 +0100

    Changed linting tasks.

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,8 @@ const config = {
 	ROOT_PATH: __dirname
 };
 
-const path = require( 'path' );
 const gulp = require( 'gulp' );
 const lintTasks = require( './lib/tasks/lint.js' )( config );
 
-// JS code sniffer.
-const jsFiles = [ path.join( config.ROOT_PATH, '**', '*.js' ) ];
-
-gulp.task( 'lint', () => lintTasks.lint( jsFiles ) );
-gulp.task( 'pre-commit', () => lintTasks.lintStaged( jsFiles ) );
+gulp.task( 'lint', lintTasks.lint );
+gulp.task( 'pre-commit', lintTasks.lintStaged );
