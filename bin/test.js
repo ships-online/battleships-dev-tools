dc30b05fa177f09b6fc8462ac3965f85315b379f
Oskar Wróbel <kontakt@oskarwrobel.pl>
2017-12-03 03:01:20 +0100

    Refactored test task from gulp to binary.

---
+++ bin/test.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const KarmaServer = require( 'karma' ).Server;
+const getKarmaConfig = require( '../lib/utils/karma.conf' );
+const parseArguments = require( '../lib/utils/parsearguments' );
+const minimist = require( 'minimist' );
+
+// Parse test options.
+const testOptions = minimist( process.argv.slice( 2 ), {
+	string: [
+		'files'
+	],
+
+	alias: {
+		f: 'files'
+	},
+
+	default: {
+		files: [ 'tests/' ]
+	}
+} );
+
+if ( typeof testOptions.files === 'string' ) {
+	testOptions.files = testOptions.files.split( ',' );
+}
+
+// Merge test options with global options.
+const options = Object.assign( testOptions, parseArguments( process.argv.slice( 2 ) ) );
+
+// Create Karma server.
+const server = new KarmaServer( getKarmaConfig( options ), exitCode => {
+	if ( exitCode !== 0 ) {
+		process.exit( exitCode );
+	}
+} );
+
+// And go :)
+server.start();
